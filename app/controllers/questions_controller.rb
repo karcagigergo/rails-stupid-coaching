a3947ebac8da22749fe4eb603aabce2ee016f5e0
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-14 01:25:41 -0300

    Done with the routing and the methods

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,26 @@
+class QuestionsController < ApplicationController
+
+    def home
+      @time = Date.today
+    end
+    
+    def answer
+      @question = params[:question]
+      @answer = coach_answer(@question)
+    end
+    
+    def ask
+    end
+
+    private
+
+    def coach_answer(question)
+      if question.downcase == "i am going to work"
+        'Great!'
+      elsif question.end_with?("?")
+        "Silly question, get dressed and go to work!"
+      else
+        "I don't care, get dressed and go to work!"
+      end
+    end
+end
